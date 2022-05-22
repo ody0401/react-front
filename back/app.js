f1ff80af299dc9ea628dcdc45cfb4142fbb74b5e
ody0401 <ody0401@naver.com>
2022-05-22 18:11:05 +0900

    4

--- back/app.js
+++ back/app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
@@ -21,8 +23,13 @@ db.sequelize.sync()
   })
   .catch(console.error);
 passportConfig();
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan('dev'));
+}
 app.use(cors({
   origin: 'http://localhost:3060',
   credentials: true,
